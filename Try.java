ab1b139f718d6c525c029d4f030e2a905a383cf3
The green arrow <1471629910@qq.com>
2019-09-12 20:19:45 +0800

    a Try

---
+++ Try.java
@@ -0,0 +1,16 @@
+public class Try{
+	public static void main(){
+		one();
+		System.out.println("tow()");
+	}
+	public static void{
+		System.out.println("34 + 30=" + 34 + 30);
+	}
+	public static int tow(){
+		int count = 10;
+		count *= count + 2;
+		count /= 100;
+		count ++;
+		return count;
+	}
+}
